7e18073937213c1fafa6add3fe79d14ba1a0a8e0
Anderson Moreira da Silva <112534079+andersonmoreirafrontend@users.noreply.github.com>
2023-11-30 15:17:21 -0300

    Create helloword.py

---
+++ helloword.py
@@ -0,0 +1,7 @@
+print("HelloWord")# Mostra na tela o texto Hello word!
+mensagem = "olá, tudo bem?"
+print(mensagem)# mostra a mensagem na tela
+print(mensagem[1]) # mostra a segunda  letra da mensagem
+print(mensagem[0:4])# mostra da primeira letra ate a 4 letra da mensagem
+print(mensagem [-1]) # mostra a ultima letra da mensagem
+print(mensagem[4::]) # mostra da quarta posição ate fim
