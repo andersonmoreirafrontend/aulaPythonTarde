e3e036afad670a3c1be9519ce1aaa1f1a2667c31
Anderson Moreira da Silva <112534079+andersonmoreirafrontend@users.noreply.github.com>
2023-11-30 17:17:15 -0300

    Create calculadoraSimples.py

---
+++ calculadoraSimples.py
@@ -0,0 +1,7 @@
+#n1 = input("Digite o primeiro numero : ")
+#n2 = input("Digite o segundo numero :  ")
+#print(int(n1)+int(n2))
+
+n1 = int(input("Digite o primeiro numero"))
+n2 = int(input("Digite o primeiro numero : "))
+print(n1+n2)
