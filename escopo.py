33845a0d8b59ece368a317f1c6d5575aaa056cf3
Anderson Moreira da Silva <112534079+andersonmoreirafrontend@users.noreply.github.com>
2023-12-05 17:11:49 -0300

    Create escopo.py

---
+++ escopo.py
@@ -0,0 +1,16 @@
+"""
+nome = "Anderson"
+print(nome)
+for x in range(3):
+    nome = "Ana"
+    print(x)
+print(nome)
+
+"""
+
+name = "Anderson"
+def MostrandoNome():
+    name = "Anderson Moreira"
+    print(name)
+MostrandoNome()
+print(name)
